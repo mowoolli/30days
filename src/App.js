d74166a5154716d8459a53a0e9f14f1a87d2e479
Marc Woolliscroft <mwoolli@gmail.com>
2020-06-25 14:21:25 -0700

    Route between home and about components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
 
 const Home = () => (
-  <div><h1>Welcome home</h1></div>
+  <div><h1>Welcome home</h1>
+    <Link to="/about">Go to about</Link></div>
+)
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <Link to="/">Go to home</Link>
+  </div>
 )
 
 class App extends React.Component {
   render() {
     return (<Router>
-      <Route path="/" component={Home} />
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/" component={Home} />
+      </Switch>
     </Router >)
   }
 }
