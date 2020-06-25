4e36474ea3fb9dbe38f6f51c853209f8cb583e4a
Marc Woolliscroft <mwoolli@gmail.com>
2020-06-25 14:11:50 -0700

    Strip app; route to stateless element

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
 import React from 'react'
-import "whatwg-fetch"
-import TimeForm from "./TimeForm"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
+const Home = () => (
+  <div><h1>Welcome home</h1></div>
+)
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.fetchCurrentTime = this.fetchCurrentTime.bind(this)
-    this.handleFormSubmit = this.handleFormSubmit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-
-    this.state = {
-      currentTime: null, areaLocation: "America/Los_Angeles", tz: "PST8PDT"
-    }
-  }
-
-  fetchCurrentTime() {
-    fetch(this.getApiUrl())
-      .then(resp => resp.json())
-      .then(resp => {
-        const currentTime = resp.datetime;
-        this.setState({ currentTime })
-      })
-  }
-  getApiUrl() {
-    const { tz, areaLocation } = this.state;
-    const host = "http://worldtimeapi.org/api/timezone"
-    return host + "/" + tz;
-  }
-  handleFormSubmit(evt) {
-    this.fetchCurrentTime();
-  }
-  handleChange(newState) {
-    this.setState(newState);
-  }
-
   render() {
-    const { currentTime, tz } = this.state
-    const apiUrl = this.getApiUrl()
-
-    return (<div>{!currentTime && <button onClick={this.fetchCurrentTime}>Get the current time</button>}{currentTime && <div>The current time is: {currentTime}</div>}<TimeForm onFormSubmit={this.handleFormSubmit} onFormChange={this.handleChange} tz={tz} areaLocation={"now"} /><p>We'll be making a request from: <code>{apiUrl}</code></p></div>)
+    return (<Router>
+      <Route path="/" component={Home} />
+    </Router >)
   }
 }
 
