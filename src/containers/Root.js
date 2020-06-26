5d9f4c71baa483d2798706759abe4f9852fca6d8
Marc Woolliscroft <mwoolli@gmail.com>
2020-06-26 10:15:33 -0700

    Add store; use provider component to make store available

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,8 +1,11 @@
 import React from "react"
 import App from "./App"
+import { Provider } from "react-redux"
+import configureStore from "../redux/configureStore"
 
 const Root = props => {
-  return <App />
+  const store = configureStore()
+  return <Provider store={store}><App /></Provider>
 }
 
 export default Root
