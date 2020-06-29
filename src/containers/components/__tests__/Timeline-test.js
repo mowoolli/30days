0c5f8fc0903d43e623bcd0500bee2b960b9694e7
Marc Woolliscroft <mwoolli@gmail.com>
2020-06-29 13:25:17 -0700

    Use enzyme to write tests for timeline component

--- src/containers/components/__tests__/Timeline-test.js
+++ src/containers/components/__tests__/Timeline-test.js
@@ -1,11 +1,18 @@
 import React from "react"
-import TestUtils from "react-dom/test-utils"
+import { shallow, mount } from "enzyme"
 
 import Timeline from "../Timeline"
 
 describe("Timeline", () => {
+  let wrapper;
+
   it("wraps content in a div with .notificationsFrame class", () => {
-    const wrapper = TestUtils.renderIntoDocument(<Timeline />)
-    const node = TestUtils.findRenderedDOMComponentWithClass(wrapper, "notificationsFrame")
+    wrapper = shallow(<Timeline />)
+    expect(wrapper.find(".notificationsFrame").length).toEqual(1)
+  })
+
+  it("has a title of Timeline", () => {
+    wrapper = mount(<Timeline />)
+    expect(wrapper.find(".title").text()).toBe("Timeline")
   })
 })
